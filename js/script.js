c0db155a744af05921c77e5291f23e05ac4d6730
Mohit Gidwani <mohit22gidwani@gmail.com>
2021-04-09 19:58:53 +0530

    added responsive ness to main page

--- js/script.js
+++ js/script.js
@@ -26,6 +26,7 @@
 //   }
 //   slides[slideIndex-1].style.display = "block";
 //   dots[slideIndex-1].className += " active-dot";
+
 // }
 
 
@@ -44,3 +45,55 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 3000); // Change image every 2 seconds
 }
+
+
+
+//  changing image in mmobile and desktop mode !!!
+imgChangeFunc = function(){
+	$(function(){
+		if($(window).width()<568){
+			// console.log($(window).width());
+			$("#img1").each(function(){
+				$(this).attr("src", $(this).attr("src").replace("images/2900 x 2003/pasta_1.jpg","images/2300x3200/fries_1.jpg"));
+			});
+
+			$("#img2").each(function(){
+				$(this).attr("src", $(this).attr("src").replace("images/2900 x 2003/chinese_1.jpg","images/2300x3200/drink_2.jpg"));
+			});
+
+			$("#img3").each(function(){
+				$(this).attr("src", $(this).attr("src").replace("images/2900 x 2003/dessert_4.jpg","images/2300x3200/dessert_1.jpg"));
+			});
+		}
+		else if ($(window).width()>=568){
+			$("#img1").each(function(){
+				$(this).attr("src", $(this).attr("src").replace("images/2300x3200/fries_1.jpg","images/2900 x 2003/pasta_1.jpg"));
+			});
+
+			$("#img2").each(function(){
+				$(this).attr("src", $(this).attr("src").replace("images/2300x3200/drink_2.jpg","images/2900 x 2003/chinese_1.jpg"));
+			});
+
+			$("#img3").each(function(){
+				$(this).attr("src", $(this).attr("src").replace("images/2300x3200/dessert_1.jpg","images/2900 x 2003/dessert_4.jpg"));
+			});
+		}
+	});
+}
+
+$(window).resize(imgChangeFunc);
+$(document).ready(imgChangeFunc);
+
+
+//  stuff for content of abut sectin !!!!!
+
+contentChangeFunc = function(){
+	if($(window).width()<992){
+		// console.log($(window).width());
+		$("div").removeClass("content-container");
+		$("div").removeClass("center");
+	}
+}
+
+$(window).resize(contentChangeFunc);
+$(document).ready(contentChangeFunc);
