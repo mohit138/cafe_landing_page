56931c057a00c09c17df37f458ec69568f3e3f0c
Mohit Gidwani <mohit22gidwani@gmail.com>
2021-04-10 23:48:44 +0530

    menu page layout done

--- js/script.js
+++ js/script.js
@@ -65,12 +65,18 @@ contentChangeFunc = function(){
 		// console.log($(window).width());
 		$("div").removeClass("content-container");
 		$("div").removeClass("center");
+		// console.log($("div"))
 	}
 
 	if($(window).width()>=992){
 		// console.log($(window).width());
-		$("#menu-container-id").addClass("content-container");
-		$("#menu-content-id").addClass("center");
+
+		// below method is used, because we wanted to detect multiple elements with same 
+		// id. if i was passing directly "#menu-container-id", it was selecting only the first element with this id
+		$("div[id='menu-container-id']").addClass("content-container");
+		$("div[id='menu-content-id']").addClass("center");
+		// console.log($("div[id='menu-container-id']"))
+		// console.log($("div[id='menu-content-id']"))
 	}
 }
 
@@ -85,12 +91,12 @@ imgChangeFunc = function(){
 		if($(window).width()<568){
 			// console.log($(window).width());
 			$("#menu-img1").each(function(){
-				$(this).attr("src", $(this).attr("src").replace("images/2900 x 2003/pasta_1.jpg","images/2300x3200/pasta_4.jpg"));
+				$(this).attr("src", $(this).attr("src").replace("../images/2900 x 2003/appetizer_1.jpg","../images/2300x3200/appetizer_2.jpg"));
 			});
 
-			// $("#menu-img2").each(function(){
-			// 	$(this).attr("src", $(this).attr("src").replace("images/2900 x 2003/chinese_1.jpg","images/2300x3200/drink_2.jpg"));
-			// });
+			$("#menu-img2").each(function(){
+				$(this).attr("src", $(this).attr("src").replace("../images/2900 x 2003/soup_1.jpg","../images/2300x3200/soup_2.jpg"));
+			});
 
 			// $("#menu-img3").each(function(){
 			// 	$(this).attr("src", $(this).attr("src").replace("images/2900 x 2003/dessert_4.jpg","images/2300x3200/dessert_1.jpg"));
@@ -98,12 +104,12 @@ imgChangeFunc = function(){
 		}
 		else if ($(window).width()>=568){
 			$("#menu-img1").each(function(){
-				$(this).attr("src", $(this).attr("src").replace("images/2300x3200/pasta_4.jpg","images/2900 x 2003/pasta_1.jpg"));
+				$(this).attr("src", $(this).attr("src").replace("../images/2300x3200/appetizer_2.jpg","../images/2900 x 2003/appetizer_1.jpg"));
 			});
 
-			// $("#img2").each(function(){
-			// 	$(this).attr("src", $(this).attr("src").replace("images/2300x3200/drink_2.jpg","images/2900 x 2003/chinese_1.jpg"));
-			// });
+			$("#menu-img2").each(function(){
+				$(this).attr("src", $(this).attr("src").replace("images/2300x3200/soup_2.jpg","images/2900 x 2003/soup_1.jpg"));
+			});
 
 			// $("#img3").each(function(){
 			// 	$(this).attr("src", $(this).attr("src").replace("images/2300x3200/dessert_1.jpg","images/2900 x 2003/dessert_4.jpg"));
