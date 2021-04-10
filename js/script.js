e764dc6a39da133d04c258a93d2395e9868115f4
Mohit Gidwani <mohit22gidwani@gmail.com>
2021-04-10 14:58:37 +0530

    added menu page, and basic layout for it

--- js/script.js
+++ js/script.js
@@ -76,3 +76,41 @@ contentChangeFunc = function(){
 
 $(window).resize(contentChangeFunc);
 $(document).ready(contentChangeFunc);
+
+
+// MENU page addition for mobile compatibility 
+
+imgChangeFunc = function(){
+	$(function(){
+		if($(window).width()<568){
+			// console.log($(window).width());
+			$("#menu-img1").each(function(){
+				$(this).attr("src", $(this).attr("src").replace("images/2900 x 2003/pasta_1.jpg","images/2300x3200/pasta_4.jpg"));
+			});
+
+			// $("#menu-img2").each(function(){
+			// 	$(this).attr("src", $(this).attr("src").replace("images/2900 x 2003/chinese_1.jpg","images/2300x3200/drink_2.jpg"));
+			// });
+
+			// $("#menu-img3").each(function(){
+			// 	$(this).attr("src", $(this).attr("src").replace("images/2900 x 2003/dessert_4.jpg","images/2300x3200/dessert_1.jpg"));
+			// });
+		}
+		else if ($(window).width()>=568){
+			$("#menu-img1").each(function(){
+				$(this).attr("src", $(this).attr("src").replace("images/2300x3200/pasta_4.jpg","images/2900 x 2003/pasta_1.jpg"));
+			});
+
+			// $("#img2").each(function(){
+			// 	$(this).attr("src", $(this).attr("src").replace("images/2300x3200/drink_2.jpg","images/2900 x 2003/chinese_1.jpg"));
+			// });
+
+			// $("#img3").each(function(){
+			// 	$(this).attr("src", $(this).attr("src").replace("images/2300x3200/dessert_1.jpg","images/2900 x 2003/dessert_4.jpg"));
+			// });
+		}
+	});
+}
+
+$(window).resize(imgChangeFunc);
+$(document).ready(imgChangeFunc);
